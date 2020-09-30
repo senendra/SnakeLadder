760d616c3ff9a9b7977ebd07491d0d11f93a977d
Senendra <senendra.007@gmail.com>
2020-09-30 19:34:52 +0530

    [Senendra] Refactor: Condition for exceeding 100 position

---
+++ SnakeLadderProgam/GameProcesser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace SnakeLadderProgam
+{
+    class GameProcesser
+    {
+        public const int NO_CHANGE = 0;
+        public const int LADDER = 1;
+        public const int SNAKE_BITE = 2;
+        public const int MAX_POSITION = 100;
+        Random random = new Random();
+        public void Ladder()
+        {
+            
+        }
+        public void gameProcesser()
+        {
+            int playerPosition = 0;
+            for (int count = 0; playerPosition < MAX_POSITION; count++)
+            {
+                int dieValue = random.Next(1, 7);
+                Console.WriteLine("Player position :" + playerPosition + " and Die Rolls gives :" + dieValue);
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case NO_CHANGE:
+                        Console.WriteLine("No Change ");
+                        break;
+                    case LADDER:
+                        if (playerPosition + dieValue > 100)
+                        {
+                            Console.WriteLine("Ladder But Exceed 100");
+                            break;
+                        }
+                        else
+                        {
+                            playerPosition = playerPosition + dieValue;
+                            Console.WriteLine("Ladder");
+                            break;
+                        }
+                    case SNAKE_BITE:
+                        if ((playerPosition - dieValue) < 0)
+                        {
+                            playerPosition = 0;
+                            break;
+                        }
+                        else
+                        {
+                            playerPosition = playerPosition - dieValue;
+                            Console.WriteLine("Snake Bite");
+                            break;
+                        }
+                }
+                Console.WriteLine("Player position after rolling Die :" + playerPosition);
+                Console.WriteLine("--------------------------------------------");
+
+            }
+        }
+    }
+}
