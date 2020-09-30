7a3978b727528546db55a341aa329364e1a72a9c
Senendra <senendra.007@gmail.com>
2020-09-30 18:55:39 +0530

    [Senendra] Refactor: Checking Option for No change, ladder and snake bite

--- SnakeLadderProgam/Program.cs
+++ SnakeLadderProgam/Program.cs
@@ -3,12 +3,31 @@ namespace SnakeLadderProgam
 {
     class Program
     {
+        public const int NO_CHANGE = 0;
+        public const int LADDER = 1;
+        public const int SNAKE_BITE = 2;
         static void Main(string[] args)
         {
             int playerPosition = 0;
             Random random = new Random();
             int dieValue = random.Next(1, 7);
             Console.WriteLine("Player position :" + playerPosition + " and Die Rolls gives :" + dieValue);
+            int check = random.Next(0, 3);
+            switch (check)
+            {
+                case NO_CHANGE:
+                    Console.WriteLine("No Change ");
+                    break;
+                case LADDER:
+                    playerPosition = playerPosition + dieValue;
+                    Console.WriteLine("Ladder");
+                    break;
+                case SNAKE_BITE:
+                    playerPosition = playerPosition - dieValue;
+                    Console.WriteLine("Snake Bite");
+                    break;
+            }
+            Console.WriteLine("Player position after rolling Die :" + playerPosition);
         }
     }
 }
