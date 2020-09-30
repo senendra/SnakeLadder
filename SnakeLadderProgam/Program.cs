d789d6d324db132a811368416bbc957a736336ec
Senendra <senendra.007@gmail.com>
2020-09-30 18:29:56 +0530

    [Senendra] Add: Initialization of player's initial position

---
+++ SnakeLadderProgam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SnakeLadderProgam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerPosition = 0;
+            Console.WriteLine("Player position :" + playerPosition);
+        }
+    }
+}
